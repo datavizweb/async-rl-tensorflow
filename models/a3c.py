d6f1e899f69c6ef8d6e8372060f83b880e72e9ff
Taehoon Kim <carpedm20@gmail.com>
2016-05-22 15:32:12 +0900

    update a3c and agent

--- models/a3c.py
+++ models/a3c.py
@@ -1,21 +1,30 @@
-import copy
+import tensorflow as tf
+
+def update_target_q_network(from_, to):
+  for name in w.keys():
+    t_w_assign_op[name].eval({t_w_input[name]: w[name].eval()})
 
 class A3C(object):
-  def __init__(self):
-    pass
+  def __init__(self, config):
+    self.global_model = model(config)
+    self.model = model(config)
 
-  def build_model(self):
-    def make_model():
-      model = A3C_FF()
-      optim = tf.train.RMSPropOptimizer(learning_rate, decay, momentum, epsilon).minimize(model.cost)
+    self.states = np.empty((self.memory_size, config.screen_height, config.screen_width), dtype = np.float16)
+    self.rewards = np.empty(self.memory_size, dtype = np.integer)
+    self.values = np.empty(self.memory_size, dtype = np.integer)
 
-      return model, optim
+    copy_weights(self.global_model, self.model)
 
-    def func(process_id):
-      env = Environment(config.env_name, config.n_action_repeat, config.max_random_start,
-          config.history_length, config.screen_height, config.screen_width)
+  def act(self, state, reward, terminal):
+    # clip reward
+    if self.max_reward:
+      reward = min(self.max_reward, reward)
+    if self.min_reward:
+      reward = max(self.min_reward, reward)
 
-      model, optim = make_model()
+    self.past_rewards[self.step - 1] = reward
 
-      dqn = NatureDQN(data_format, config.history_length,
-          config.screen_height, config.screen_width, env.action_size, name='nature')
+    if terminal:
+      R = 0
+    else:
+      _, vout = self.
---
+++ models/agent.py
@@ -0,0 +1,55 @@
+import copy
+import multiprocessing as mp
+
+class Agent(object):
+  def __init__(self):
+    t = mp.Value('t', 0)
+    pass
+
+  def make_env(self):
+    return Environment(config.env_name, config.n_action_repeat, config.max_random_start,
+        config.history_length, config.screen_height, config.screen_width)
+
+  def build_model(self):
+    def make_model():
+      model = A3C_FF()
+      optim = tf.train.RMSPropOptimizer(learning_rate, decay, momentum, epsilon).minimize(model.cost)
+
+      return model, optim
+
+    env = self.make_env()
+
+    def func(env, process_id):
+      model, optim = make_model()
+
+      dqn = NatureDQN(data_format, config.history_length,
+          config.screen_height, config.screen_width, env.action_size, name='nature')
+
+  def train(self):
+    while:
+      with counter.get_lock():
+        counter.value += 1
+        global_t = counter.value
+
+      if global_t > step:
+        break
+
+      agent.optim.lr = (max_step - global_t - 1) / max_step * args.lr
+
+      action = agent.act(env.
+
+
+  def evaluation(self, func, n_runs):
+    rewards = []
+    for i in xrange(n_runs):
+      current_reward = 0
+      env = self.make_env()
+
+      state, reward, terminal = env.new_random_game()
+      while not terminal:
+        state, reward, terminal = env.step(action)
+        current_reward += reward
+
+      rewards.append(current_reward)
+
+    return rewards
