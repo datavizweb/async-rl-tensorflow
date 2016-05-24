590dc5a2d90b34c85a951f78c3f399fe627c162f
Taehoon Kim <carpedm20@gmail.com>
2016-05-24 18:39:33 +0900

    accumulate gradients in progress

--- models/a3c.py
+++ models/a3c.py
@@ -1,8 +1,26 @@
 import tensorflow as tf
 
+from .deep_q_network import AsyncNetwork
+
 def update_target_q_network(from_, to):
   for name in w.keys():
     t_w_assign_op[name].eval({t_w_input[name]: w[name].eval()})
+
+def accumulate_gradients(tower_grads):
+  accumulate_grads = []
+  for grad_and_vars in zip(*tower_grads):
+    grads = []
+    for g, _ in grad_and_vars:
+      expanded_g = tf.expand_dims(g, 0)
+      grads.append(expanded_g)
+
+    grad = tf.concat(0, grads)
+    grad = tf.reduce_sum(grad, 0)
+
+    v = grad_and_vars[0][1]
+    grad_and_var = (grad, v)
+    accumulate_grads.append(grad_and_var)
+  return accumulate_grads
 
 class A3C_FF(object):
   def __init__(self):
@@ -21,10 +39,12 @@ class A3C_FF(object):
     copy_weights(self.global_model, self.model)
 
   def build_model(self):
-    policy_grads, value_grads = [], []
+    self.networks, policy_grads, value_grads = [], [], []
     for step in xrange(self.max_step):
       with tf.name_scpe('A3C_%d' % step) as scope:
+        self.networks = AsyncNetwork()
 
+        # Share parameters between networks
         tf.get_variable_scope().reuse_variables()
 
         policy_grad = self.policy_optim.compute_gradients(self.policy_loss)
@@ -33,9 +53,9 @@ class A3C_FF(object):
         policy_grads.append(policy_grad)
         value_grads.append(value_grad)
 
-        accumulated_grads = []
-        for grads in [policy_grads, value_grad]:
-          for grad_and_vars in zip(*grads):
+    # Accumulate gradients for n-steps
+    accumulated_policy_grads = accumulate_gradients(policy_grads)
+    accumulated_value_grads = accumulate_gradients(value_grads)
 
   def act(self, s_t, reward, terminal):
     self.learning_rate = (max_step - global_t - 1) / max_step * args.learning_rate
