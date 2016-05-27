now gradient accumulation finished. code execution error need to be fixed

main.py
@@ -50,18 +50,23 @@ logger.setLevel(config.log_level)
 tf.set_random_seed(config.random_seed)
 random.seed(config.random_seed)
 
+global_t = 0
+should_stop = False
+
 def main(_):
   with tf.Session() as sess:
-    global_network = DeepQNetwork(config.data_format,
-                                  config.history_length,
-                                  config.screen_height,
-                                  config.screen_width,
-                                  gym.make(config.env_name).action_space.n)
-    global_optim = tf.train.RMSPropOptimizer(config.learning_rate, config.decay, config.momentum, config.epsilon)
+    with tf.variable_scope('master') as scope:
+      global_network = DeepQNetwork(config.data_format,
+                                    config.history_length,
+                                    config.screen_height,
+                                    config.screen_width,
+                                    gym.make(config.env_name).action_space.n)
+      global_optim = tf.train.RMSPropOptimizer(config.learning_rate, config.decay, config.momentum, config.epsilon)
 
     global_t = 0
-    thread_stop = False
+
     def train_function(idx):
+      global global_t
       model = models[idx]
       state, reward, terminal = model.env.new_random_game()
 
@@ -69,21 +74,31 @@ def main(_):
         diff_global_t = model.act(state, reward, terminal)
         global_t += diff_global_t
 
+    # Define thread-specific models
     models = []
     for thread_id in range(config.n_thread):
       model = A3C_FF(thread_id, config, sess, global_network, global_optim)
       models.append(model)
 
+    # Initialize and load model weights
     tf.initialize_all_variables().run()
-    saver = tf.train.Saver(global_network.w.values(), max_to_keep=30)
 
+    per_thread_steps = [model.optim_step for model in models]
+    saver = tf.train.Saver(global_network.w.values() + per_thread_steps, max_to_keep=30)
+
+    # copy weights from the global models
     for model in models:
       model.copy_from_global()
 
+    # Prepare each threads to run asynchronously
     threads = []
     for idx in range(config.n_thread):
       threads.append(Thread(target=train_function, args=(idx,)))
 
+    # Test for syncrhnous training
+    train_function(0)
+
+    # Execute and wait for the end of the training
     for thread in threads:
       thread.start()
 

models/a3c.py
@@ -30,23 +30,26 @@ class A3C_FF(object):
     self.max_reward = config.max_reward
     self.min_reward = config.min_reward
 
-    self.data_format = config.data_format
-    self.history_length = config.history_length
     self.screen_height = config.screen_height
     self.screen_width = config.screen_width
+    self.data_format = config.data_format
+    self.history_length = config.history_length
 
-    self.s_ts = np.empty((self.n_step, config.screen_height, config.screen_width), dtype = np.float16)
-    self.prev_rewards = np.empty(self.n_step, dtype = np.integer)
-    self.prev_values = np.empty(self.n_step, dtype = np.integer)
+    self.prev_s = np.empty((self.n_step, self.history_length, config.screen_height, config.screen_width), dtype=np.float16)
+    self.prev_r = np.empty(self.n_step, dtype=np.integer)
+    self.prev_v = np.empty(self.n_step, dtype=np.integer)
+    self.prev_t = np.empty(self.n_step, dtype=np.bool)
 
     self.build_model()
 
   def build_model(self):
     self.networks, grads = [], []
 
-    with tf.variable_scope('t%d' % self.thread_id) as scope:
+    with tf.variable_scope('thread%d' % self.thread_id) as scope:
+      self.optim_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+
       for step in xrange(self.n_step):
-        with tf.variable_scope('A3C_%d' % step) as scope:
+        with tf.name_scope('A3C_%d' % step) as scope:
           network = DeepQNetwork(self.data_format,
                                 self.history_length,
                                 self.screen_height,
@@ -68,26 +71,7 @@ class A3C_FF(object):
           summaries.append(
               tf.histogram_summary('%s/gradients' % var.op.name, grad))
 
-      self.optim_step = tf.placeholder('int32', None, name='optim_step')
-
-      import ipdb; ipdb.set_trace() 
-      self.apply_gradeint_op = self.global_optim.apply_gradients(
-          self.global_model.variables, accumulated_grads)
-
-      if False: #self.pid == 0:
-        for var in tf.trainable_variables():
-          summaries.append(tf.histogram_summary(var.op.name, var))
-
-        # Track the moving averages of all trainable variables.
-        variable_averages = tf.train.ExponentialMovingAverage(
-            cifar10.MOVING_AVERAGE_DECAY, global_step)
-        variables_averages_op = variable_averages.apply(tf.trainable_variables())
-
-        self.train_op = tf.group(
-            self.policy_apply_gradeint_op, self.value_apply_gradient_op, variables_averages_op)
-
-        # Create a saver.
-        saver = tf.train.Saver(tf.all_variables())
+      self.apply_gradeint_op = self.global_optim.apply_gradients(accumulated_grads, global_step=self.optim_step)
 
   def act(self, s_t, reward, terminal):
     logger.info(" [%d] ACT : %s, %s" % (self.thread_id, reward, terminal))
@@ -98,7 +82,7 @@ class A3C_FF(object):
     if self.min_reward:
       reward = max(self.min_reward, reward)
 
-    self.prev_rewards[self.t - 1] = reward
+    self.prev_r[self.t - 1] = reward
 
     if (terminal and self.t_start < self.t) or self.t - self.t_start == self.t_max:
       r, a, s = {}, {}, {t: s_t}
@@ -116,7 +100,7 @@ class A3C_FF(object):
 
       self.sess.run(self.self.train_op, feed_dict=data)
     else:
-      Q = self.sess.run([self.networks[0].value], {self.networks[0].s_t: s_t})
+      Q = self.sess.run([self.networks[0].value], {self.networks[0].s_t: [s_t]})
 
     if not terminal:
       self.prev_s[self.t] = s_t
@@ -126,6 +110,8 @@ class A3C_FF(object):
     else:
       print "Should implement"
       pass
+
+    return self.start_t - self.t
 
   def copy_from_global(self):
     for network in self.networks:

models/deep_q_network.py
@@ -29,17 +29,9 @@ class DeepQNetwork(object):
       self.l4, self.w['l4_w'], self.w['l4_b'] = \
           linear(self.l3, 512, activation_fn=activation_fn, name='l4_linear')
 
-    with tf.variable_scope('copy_from_target'):
-      self.w_input = {}
-      self.w_assign_op = {}
-
-      for name in self.w.keys():
-        self.w_input[name] = tf.placeholder('float32', self.w[name].get_shape().as_list(), name=name)
-        self.w_assign_op[name] = self.w[name].assign(self.w_input[name])
-
     with tf.variable_scope('policy'):
       # 512 -> action_size
-      self.logits, self.w['p_w'], self.w['p_b'] = linear(self.l4, action_size, name='logits')
+      self.logits, self.w['p_w'], self.w['p_b'] = linear(self.l4, action_size, name='linear')
 
       self.policy = tf.nn.softmax(self.logits, name='pi')
       self.log_policy = tf.log(tf.nn.softmax(self.logits))
@@ -52,22 +44,27 @@ class DeepQNetwork(object):
 
     with tf.variable_scope('value'):
       # 512 -> 1
-      self.value, self.w['q_w'], self.w['q_b'] = linear(self.l4, 1, name='V')
+      self.value, self.w['q_w'], self.w['q_b'] = linear(self.l4, 1, name='linear')
 
     with tf.variable_scope('optim'):
       self.R = tf.placeholder('float32', [None], name='target_reward')
 
-      with tf.variable_scope('policy'):
-        self.action = tf.placeholder('int64', [None], name='action')
+      self.action = tf.placeholder('int64', [None], name='action')
+      action_one_hot = tf.one_hot(self.action, action_size, 1.0, 0.0, name='action_one_hot')
 
-        action_one_hot = tf.one_hot(self.action, action_size, 1.0, 0.0, name='action_one_hot')
-        self.policy_loss = tf.reduce_sum(self.log_policy * action_one_hot, 1) \
-            * (self.R - self.value + beta * self.entropy)
-
-      with tf.variable_scope('value'):
-        self.value_loss = tf.pow(self.R - self.value, 2)
+      self.policy_loss = tf.reduce_sum(self.log_policy * action_one_hot, 1) \
+          * (self.R - self.value + beta * self.entropy)
+      self.value_loss = tf.pow(self.R - self.value, 2)
 
       self.total_loss = self.policy_loss + self.value_loss
+
+    with tf.variable_scope('copy_from_target'):
+      self.w_input = {}
+      self.w_assign_op = {}
+
+      for name in self.w.keys():
+        self.w_input[name] = tf.placeholder('float32', self.w[name].get_shape().as_list(), name=name)
+        self.w_assign_op[name] = self.w[name].assign(self.w_input[name])
 
   def calc_policy_value(self, s_t):
     return self.sess.run([self.policy, self.value], {self.s_t: s_t})